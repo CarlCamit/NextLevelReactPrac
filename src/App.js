18f9ff681b11542fa1b7b42424aa505e8cca8ff5
Carl Camit <carlcamit0@gmail.com>
2017-11-28 12:07:22 +1100

    moved data into an array and use map function to display data

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import ToDoItem from './components/Todoitem'
 
+// Data
+const items = [
+  { description: "First", completed: true },
+  { description: "Second", completed: true },
+  { description: "Third", completed: false }
+]
+
+// UI
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ToDoItem description="First" completed/>
-        <ToDoItem description="Second" />
-        <ToDoItem description="Third" />
+        {
+          items.map((item) => (
+            <ToDoItem description={ item.description } completed={ item.completed } />
+          ))
+        }
       </div>
     );
   }
